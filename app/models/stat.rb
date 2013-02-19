2e28beba95a45a4f198cf5ac54828ceb1190ec29
Andrew Curley <andrew.curley@gmail.com>
2013-02-19 17:06:24 -0500

    Add database and model work for the CoreLogic report

--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,6 +1,7 @@
 class Stat < ActiveRecord::Base
   attr_accessible :date_retrieved, :current_occupied, :total_vacants, :vacant_rented, :vacant_unrented, 
-                  :percent_occupied, :percent_preleased, :phases, :total_guest_cards, :total_apps
+                  :percent_occupied, :percent_preleased, :phases, :total_guest_cards, :total_apps, :application_accepted,
+                  :application_accepted_with_conditions, :applications_declined, :average_application_score, :rejection_rate
 
   belongs_to :property
 
